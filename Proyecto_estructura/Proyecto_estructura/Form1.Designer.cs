eaed9da9e6b044be217c32d0fc03612d642deea0
Thiago <tsosachugar@gmial.com>
2024-10-29 19:52:23 -0400

    Se cambia el gui

--- Proyecto_estructura/Proyecto_estructura/Form1.Designer.cs
+++ Proyecto_estructura/Proyecto_estructura/Form1.Designer.cs
@@ -33,19 +33,22 @@
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(246, 181);
+            this.button1.BackColor = System.Drawing.Color.Cornsilk;
+            this.button1.ForeColor = System.Drawing.Color.CornflowerBlue;
+            this.button1.Location = new System.Drawing.Point(88, 68);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(297, 80);
             this.button1.TabIndex = 0;
-            this.button1.Text = "button1";
-            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Text = "Prueba";
+            this.button1.UseVisualStyleBackColor = false;
             this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.BackColor = System.Drawing.SystemColors.ControlLight;
+            this.ClientSize = new System.Drawing.Size(458, 216);
             this.Controls.Add(this.button1);
             this.Name = "Form1";
             this.Text = "Form1";
