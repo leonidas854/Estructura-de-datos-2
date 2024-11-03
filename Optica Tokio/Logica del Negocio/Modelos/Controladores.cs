7c90d06736191252705c6712c1bf08dcd7973181
Leo <tsossachugar@gmial.com>
2024-11-03 02:35:34 -0400

    Modifique estructuras

---
+++ Optica Tokio/Logica del Negocio/Modelos/Controladores.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Optica_Tokio.Logica_del_Negocio.Modelos
+{
+    class Controladores
+    {
+    }
+}
