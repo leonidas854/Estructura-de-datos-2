ce67188478a83934772f56639c88a1c65c0055a6
Leo <tsossachugar@gmial.com>
2024-11-03 23:53:15 -0400

    interfaces de login y carga

--- Optica Tokio/UI/Formularios/Frm LoadingScreen.cs
+++ Optica Tokio/UI/Formularios/Frm LoadingScreen.cs
@@ -16,5 +16,39 @@ namespace Optica_Tokio.UI.Formularios
         {
             InitializeComponent();
         }
+       
+
+        private void TmIn_Tick(object sender, EventArgs e)
+        {
+            if (this.Opacity < 1) { this.Opacity += 0.05; }
+            cpbtiempo.Value += 1;
+            cpbtiempo.Text = cpbtiempo.Value.ToString();
+            if(cpbtiempo.Value == 100)
+            {
+                TmIn.Stop();
+                Tmclose.Start();
+            }
+            
+        }
+
+        private void Tmclose_Tick(object sender, EventArgs e)
+        {
+            this.Opacity -=0.1;
+            if(this.Opacity == 0)
+            {
+                Tmclose.Stop();
+                this.Close();
+            }
+
+        }
+
+        private void FrmLoading_Screen_Load(object sender, EventArgs e)
+        {
+            this.Opacity = 0.0;
+            cpbtiempo.Value = 0;
+            cpbtiempo.Minimum = 0;
+            cpbtiempo.Maximum = 100;
+            TmIn.Start();
+        }
     }
 }
