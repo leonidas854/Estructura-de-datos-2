1998a6845dc18e3e945e9352a12836a60c16a474
Leo <tsossachugar@gmial.com>
2024-11-07 14:33:31 -0400

    solucionado

--- Optica Tokio/UI/Formularios/Frm User.Designer.cs
+++ Optica Tokio/UI/Formularios/Frm User.Designer.cs
@@ -32,11 +32,9 @@
             // 
             // Frm_User
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1312, 690);
-            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
-            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.ClientSize = new System.Drawing.Size(984, 561);
             this.Name = "Frm_User";
             this.Text = "User";
             this.ResumeLayout(false);
